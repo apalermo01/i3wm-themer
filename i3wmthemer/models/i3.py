3631272cc9f7ede041115023df5e0af7ed4cb733
alex <alex.palermo.45@gmail.com>
2023-02-06 18:24:58 -0500

    bugfixes, adjusted colors for theme 007:

--- i3wmthemer/models/i3.py
+++ i3wmthemer/models/i3.py
@@ -35,48 +35,62 @@ class I3Theme(AbstractTheme):
         focused_inactive_list = ['foreground', 'background', 'foreground', 'color4', 'color4']
         urgent_list =  ['foreground', 'background', 'foreground', 'color4', 'color4']
         placeholder_list =  ['foreground', 'background', 'foreground', 'color4', 'color4']
-
+        print("i3theme keys = ", self.i3theme.keys())
         if "colors" not in self.i3theme:
-            self.background = self.x_resources[XresourcesAttr.BACKGROUND.value]
-            self.focused = " ".join([self.x_resources[i] for i in focused_list])
-            self.unfocused = " ".join([self.x_resources[i] for i in unfocused_list])
-            self.inactive = " ".join([self.x_resources[i] for i in focused_inactive_list])
-            self.urgent = " ".join([self.x_resources[i] for i in urgent_list])
-            self.placeholder = " ".join([self.x_resources[i] for i in placeholder_list])
+            self.i3theme['colors'] = self.i3theme
+            # self.background = self.x_resources[XresourcesAttr.BACKGROUND.value]
+            # self.focused = " ".join([self.x_resources[i] for i in focused_list])
+            # self.unfocused = " ".join([self.x_resources[i] for i in unfocused_list])
+            # self.inactive = " ".join([self.x_resources[i] for i in focused_inactive_list])
+            # self.urgent = " ".join([self.x_resources[i] for i in urgent_list])
+            # self.placeholder = " ".join([self.x_resources[i] for i in placeholder_list])
+        if 'background' in self.i3theme['colors']:
+            self.background = self.parse_color_line(self.i3theme['colors']['background'], self.x_resources)
+            print("USING COLORS.BACKGROUND")
+        elif 'client.background' in self.i3theme['colors']:
+            self.background = self.parse_color_line(self.i3theme['colors']['client.background'], self.x_resources)
+            print("USING CLIENT.BACKGROUND")
         else:
-            if 'background' in self.i3theme['colors']:
-                self.background = self.parse_color_line(self.i3theme['colors']['background'], self.x_resources)
-            else:
-                self.background = self.x_resources[XresourcesAttr.BACKGROUND.value]
+            self.background = self.x_resources[XresourcesAttr.BACKGROUND.value]
+            print("USING DEFAULT BACKGROUND")
+        if 'focused' in self.i3theme['colors']:
+            self.focused = self.parse_color_line(self.i3theme['colors']['focused'], self.x_resources)
+        elif 'client.focused' in self.i3theme['colors']:
+            self.focused = self.parse_color_line(self.i3theme['colors']['client.focused'], self.x_resources)
+        else:
+            self.focused = " ".join([self.x_resources[i] for i in focused_list])
 
-            if 'focused' in self.i3theme['colors']:
-                self.focused = self.parse_color_line(self.i3theme['colors']['focused'], self.x_resources)
-            else:
-                self.focused = " ".join([self.x_resources[i] for i in focused_list])
+        if 'unfocused' in self.i3theme['colors']:
+            self.unfocused = self.parse_color_line(
+                                self.i3theme['colors']['unfocused'],
+                                self.x_resources)
+        elif 'client.unfocused' in self.i3theme['colors']:
+            self.unfocused = self.parse_color_line(self.i3theme['colors']['client.unfocused'], self.x_resources)
+        else:
+            self.unfocused = " ".join([self.x_resources[i] for i in unfocused_list])
 
-            if 'unfocused' in self.i3theme['colors']:
-                self.unfocused = self.parse_color_line(
-                                    self.i3theme['colors']['unfocused'],
-                                    self.x_resources)
-            else:
-                self.unfocused = " ".join([self.x_resources[i] for i in unfocused_list])
+        if 'focused_inactive' in self.i3theme['colors']:
+            self.inactive = self.parse_color_line(
+                                self.i3theme['colors']['focused_inactive'],
+                                self.x_resources)
+        elif 'client.focused_inactive' in self.i3theme['colors']:
+            self.inactive = self.parse_color_line(self.i3theme['colors']['client.focused_inactive'], self.x_resources)
+        else:
+            self.inactive = " ".join([self.x_resources[i] for i in focused_inactive_list])
 
-            if 'focused_inactive' in self.i3theme['colors']:
-                self.inactive = self.parse_color_line(
-                                    self.i3theme['colors']['focused_inactive'],
-                                    self.x_resources)
-            else:
-                self.inactive = " ".join([self.x_resources[i] for i in focused_inactive_list])
+        if 'urgent' in self.i3theme['colors']:
+            self.urgent = self.parse_color_line(self.i3theme['colors']['urgent'], self.x_resources)
+        elif 'client.urgent' in self.i3theme['colors']:
+            self.urgent = self.parse_color_line(self.i3theme['colors']['client.urgent'], self.x_resources)
+        else:
+            self.urgent = " ".join([self.x_resources[i] for i in urgent_list])
 
-            if 'urgent' in self.i3theme['colors']:
-                self.urgent = self.parse_color_line(self.i3theme['colors']['urgent'], self.x_resources)
-            else:
-                self.urgent = " ".join([self.x_resources[i] for i in urgent_list])
-
-            if 'placeholder' in self.i3theme['colors']:
-                self.placeholder = self.parse_color_line(self.i3theme['colors']['placeholder'], self.x_resources)
-            else:
-                self.placeholder = " ".join([self.x_resources[i] for i in placeholder_list])
+        if 'placeholder' in self.i3theme['colors']:
+            self.placeholder = self.parse_color_line(self.i3theme['colors']['placeholder'], self.x_resources)
+        elif 'client.placeholder' in self.i3theme['colors']:
+            self.placeholder = self.parse_color_line(self.i3theme['colors']['client.placeholder'], self.x_resources)
+        else:
+            self.placeholder = " ".join([self.x_resources[i] for i in placeholder_list])
 
     def load(self, configuration):
         """Load settings into i3 config file.
